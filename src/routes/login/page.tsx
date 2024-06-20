a903c8a7081c8fd2af43068629a5a9b20378c8db
DJWL <1602951127@qq.com>
2024-06-20 17:11:38 +0800

    feat: 登录完成

--- src/routes/login/page.tsx
+++ src/routes/login/page.tsx
@@ -1,5 +1,52 @@
+import { Form, Button, Toast } from '@douyinfe/semi-ui';
+import type { FormApi as SFormApi } from '@douyinfe/semi-ui/lib/es/form';
+import { useState } from 'react';
+import { useNavigate } from '@modern-js/runtime/router';
+import { login } from '@/services/user';
+
+const { Input } = Form;
 function Login() {
-  return <div>登录组件</div>;
+  const [formApi, setFormApi] = useState<SFormApi>();
+  const navigate = useNavigate();
+  const redirect = new URLSearchParams(window.location.search)?.get('redirect');
+
+  const submit = async () => {
+    try {
+      const res = await formApi?.validate();
+      const result = await login(res);
+      if (result.data?.code === 200) {
+        const { token } = result.data?.data || {};
+        localStorage.setItem('token', token);
+        if (redirect) {
+          navigate(redirect);
+        } else {
+          navigate('/');
+        }
+        Toast.success('登录成功');
+      }
+    } catch (e) {}
+  };
+  return (
+    <div
+      style={{
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        gap: 16,
+        paddingTop: 50,
+      }}
+    >
+      <Form getFormApi={setFormApi} style={{ width: 200 }}>
+        <Input field="username" label="用户名" />
+        <Input field="password" label="密码" />
+      </Form>
+      <Button onClick={submit} style={{ width: 200 }}>
+        登录
+      </Button>
+    </div>
+  );
 }
 
 export default Login;
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,11 +1,33 @@
 import axios from 'axios';
+import { cloneDeep } from 'lodash-es';
 import { ENV } from './env';
 
 const request = axios.create({
   baseURL: ENV.isProd ? undefined : 'http://127.0.0.1:8419',
-  headers: {
-    Authorization: 'Basic a2Fuc29rdToxMjM0NTY=',
-  },
 });
+
+request.interceptors.request.use(config => {
+  const headers = cloneDeep(config.headers || {});
+  const token = localStorage.getItem('token');
+  if (token) {
+    headers.Authorization = token;
+  }
+  config.headers = headers;
+  return config;
+});
+
+request.interceptors.response.use(
+  response => {
+    return response;
+  },
+  error => {
+    const { response } = error;
+    if (response.status === 401) {
+      localStorage.removeItem('token');
+      const path = window.location.pathname;
+      window.location.href = `/login?redirect=${path}`;
+    }
+  },
+);
 
 export default request;
